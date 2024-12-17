5614334e1a4514d2d4f9d9dfce39f5232401a63d
NituKumari95080 <nitukumari251001@gmail.com>
2024-12-17 23:26:54 +0530

    add nav and home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Nav from './componets/Nav/Nav'
+import Home from './componets/Home/Home'
 
 const App = () => {
   return (
-    <div>
-      house
-    </div>
+    <>
+      <Nav/>
+      <Home/>
+    </>
   )
 }
 
---
+++ src/componets/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+const Nav = () => {
+  return (
+    <div id='nav'>
+      <div className="nav1"></div>
+      <div className="nav2"></div>
+    </div>
+  )
+}
+
+export default Nav
